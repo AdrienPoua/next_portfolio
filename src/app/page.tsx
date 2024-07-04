4392e242a84743008ccdc90c9691cda421b8647f
zartax95 <adrienvilb@msn.com>
2024-07-04 19:00:41 +0200

    architecture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
-import Image from "next/image";
+import { Container } from "@mui/material";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import HeroSection from "@components/HeroSection";
+import About from "@components/About";
+import Skills from "@/components/Skills";
+import Projets from "@components/Projets";
+import Contact from "@components/Contact";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl font-bold text-center">
-        Bienvenue sur mon portfolio !
-      </h1>
-      <Image
-        src="/images/illustration.svg"
-        alt="Illustration"
-        width={500}
-        height={500}
-      />
-    </main>
+    <>
+      <Header />
+      <Container>
+        <Container component="main">
+          <HeroSection />
+        </Container>
+        <About />
+        <Skills />
+        <Projets />
+        <Contact />
+      </Container>
+      <Footer />
+    </>
   );
 }
--- src/app/theme.tsx
+++ src/app/theme.tsx
@@ -3,53 +3,6 @@ import { createTheme } from "@mui/material/styles";
 
 // Créez un thème personnalisé
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#1976d2",
-      dark: "#004ba0",
-    },
-    secondary: {
-      main: "#ee6730",
-    },
-  },
-  typography: {
-    fontFamily: "Evogria, Roboto",
-    body1: {
-      color: "#fff",
-    },
-    body2: {
-      color: "#000",
-    },
-    h1: {
-      color: "#ffffff",
-      textAlign: "center",
-      fontSize: "3rem",
-      marginTop: "2.5rem",
-      marginBottom: "5rem",
-    }
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          "&.Mui-disabled": {
-            backgroundColor: "gray",
-            color: "white",
-            borderColor: "gray",
-          },
-        },
-      },
-    },
-    MuiCardContent: {
-      styleOverrides: {
-        root: {
-          '&:last-child': {
-            paddingBottom: 0, 
-          }
-        },
-      },
-    },
-  },
 });
 
 export default theme;
