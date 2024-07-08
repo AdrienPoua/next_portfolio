8f494521762ef481dbc242454b10d9322df4a468
zartax95 <adrienvilb@msn.com>
2024-07-08 13:36:25 +0200

    styles : mobile navbar add icon

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 import { Box, Container, Button, Drawer, List, ListItem } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import BlurCircularIcon from '@mui/icons-material/BlurCircular';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import DevicesIcon from '@mui/icons-material/Devices';
 
 type CustomLinkProps = {
   href: string,
   title: string,
+  endIcon: React.ReactNode,
   setIsOpen: (x: boolean) => void
 }
 
-const CustomLink = ({ href, title, setIsOpen }: CustomLinkProps) => {
+const CustomLink = ({ href, title, setIsOpen, endIcon }: CustomLinkProps) => {
   return (
     <ListItem className="p-0">
-        <Button href={"#"+href} onClick={() => setIsOpen(false)} className="text-black size-full rounded-none py-6" variant="contained">
-          {title}
-        </Button>
+      <Button href={"#" + href} onClick={() => setIsOpen(false)} className="text-black size-full rounded-none py-6 " variant="contained" endIcon={endIcon}>
+        {title}
+      </Button>
     </ListItem>
   );
 }
@@ -31,10 +36,14 @@ export default function MobileNav() {
         onClose={() => setIsOpen(false)}>
         <Box>
           <List className="flex flex-col p-0 min-w-[200px]">
-            <CustomLink href="about" title="About" setIsOpen={setIsOpen} />
-            <CustomLink href="compétences" title="Compétences" setIsOpen={setIsOpen} />
-            <CustomLink href="projets" title="projects" setIsOpen={setIsOpen} />
-            <CustomLink href="contact" title="Contact" setIsOpen={setIsOpen} />
+            <CustomLink href="about" title="À propos" setIsOpen={setIsOpen} endIcon={<PersonOutlineIcon />
+            } />
+            <CustomLink href="compétences" title="Compétences" setIsOpen={setIsOpen} endIcon={<BlurCircularIcon />
+            } />
+            <CustomLink href="projets" title="projets" setIsOpen={setIsOpen} endIcon={<DevicesIcon />
+            } />
+            <CustomLink href="contact" title="Contact" setIsOpen={setIsOpen} endIcon={<MailOutlineIcon />
+            } />
           </List>
         </Box>
       </Drawer>
