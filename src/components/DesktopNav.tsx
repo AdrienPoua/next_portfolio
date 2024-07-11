3fd2e1186d903f8a7386560b49c402731660aea7
zartax95 <adrienvilb@msn.com>
2024-07-11 11:33:42 +0200

    styles : styles change

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -1,17 +1,18 @@
-import { Box, Link } from '@mui/material';
+import { Box, Link, Typography } from '@mui/material';
 import React from 'react';
 
 const CustomLink = ({ href, children }: { href: string, children: React.ReactNode }) => {
   return (
-    <Link href={href} color="black" underline="hover" className="mr-4 text-xl underline-offset-4 ">
-      {children}
+    <Link href={href} underline='none' className="relative group mr-4 text-xl overflow-hidden rounded-lg px-8 py-3 ">
+      <Box className="bg-black absolute -left-3 -right-3  top-0 bottom-0 translate-x-full -skew-x-12 transition-transform duration-300 group-hover:translate-x-0 group-hover:ease-in-out -z-10" />
+      <Typography className="relative text-black group-hover:text-white duration-300 text-xl ease-in-out">{children}</Typography>
     </Link>
   );
 };
 
 export default function DesktopNav() {
   return (
-    <Box component="nav" className="py-5 px-10 md:mb-28">
+    <Box component="nav" className="mt-3 px-10 md:mb-28">
       <Box className="flex items-center justify-end gap-4">
         <CustomLink href="#about">À propos</CustomLink>
         <CustomLink href="#compétences">Compétences</CustomLink>
