8747a1ae3e9bb6db9574468be901343e3556486c
zartax95 <adrienvilb@msn.com>
2024-07-13 10:10:01 +0200

    styles : mb layout

--- src/layouts/Section.tsx
+++ src/layouts/Section.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function Section({ title, children, black, href }: Readonly<{ title: string, children: React.ReactNode, black?: boolean, href?: string }>) {
     return (
-        <Box component="section" className={`flex flex-col py-16 px-[10%] ${black ? "bg-black" : ""}`} id={href ? href : title.toLowerCase().replace(/\s+/g, '-')}>
+        <Box component="section" className={`flex flex-col pt-20 pb-32 px-[10%] ${black ? "bg-black" : ""}`} id={href ? href : title.toLowerCase().replace(/\s+/g, '-')}>
             <Box className="flex items-center mb-16">
                 <Box className={`w-8 h-6 inline-block shrink-0 ${black ? "bg-white" : "bg-black"}`} />
                 <Typography variant="h2" className={`ms-4 text-4xl md:text-5xl break-all  ${black ? "text-white" : "text-black"} `}>{title}</Typography>
