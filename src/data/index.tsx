54fe8429e50fb864498328ec0f3645a775ad3abb
zartax95 <adrienvilb@msn.com>
2025-02-28 18:09:03 +0100

    add vizir domain

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -2,7 +2,7 @@ export const projectsData = [
   {
     title: "Argenteuil Basketball",
     description: "Site officiel du club de basket d'Argenteuil. Fait de A à Z from scratch.",
-    tags: ["Next.js", "MongoDB", "Tailwind", "Sanity", "Shadcn", "Next-auth", "Design patterns", "API"],
+    tags: ["Next.js", "MongoDB", "Tailwind", "Sanity", "Shadcn", "Next-auth", "API"],
     imageUrl: "/ABB.png",
     site: "https://argenteuilbasketball.com",
     github: "https://github.com/AdrienPoua/argenteuil_basketball",
@@ -12,7 +12,7 @@ export const projectsData = [
     description: "Site de location d'un appartement à Marrakech pour un particulier.",
     tags: ["Next.js", "Stripe", "Shadcn", "Tailwind"],
     imageUrl: "/vizir.png",
-    site: "https://marrakech-three.vercel.app",
+    site: "https://www.vizirmarrakech.com/",
     github: "https://github.com/AdrienPoua/Vizir",
   },
   {
